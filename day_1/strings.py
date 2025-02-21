e0ff8b9f285f34ac5947ffeb5bfc8add3918cdf9
marosirobi <marosirobi666@gmail.com>
2025-02-21 23:22:24 +0100

    day 1.2

---
+++ day_1/strings.py
@@ -0,0 +1,15 @@
+print("hello world")
+print("1. Mix 500g of Flour,\n10g Yeast and 300ml Water in a bowl.")
+print("hello" + "world\n")
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \\ and the letter n\n")
+
+input("enter your name: ")
+
+# ctrl + /
+print("hello " + input("enter your name: ") +"!")
+
