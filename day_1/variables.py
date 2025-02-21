357f3ada475ea7b237f58c2280d225d7ca604f99
marosirobi <marosirobi666@gmail.com>
2025-02-21 23:23:02 +0100

    day 1.2

---
+++ day_1/variables.py
@@ -0,0 +1,21 @@
+name = input("enter your name: ")
+print(len(name))
+name = "Péter"
+print(name)
+
+# print(len(input("enter your name: ")))
+
+username = input("enter your name: ")
+length = len(username)
+
+print(length)
+
+glass1 = "milk"
+glass2 = "juice"
+
+glass3 = glass1
+glass1 = glass2
+glass2 = glass3
+
+print(glass1)
+print(glass2)
